6471ddbbaaa09dd0a9f6ee6c645fc8ee7c080263
Nikita <nikita.polischukov@gmail.com>
2026-01-14 10:35:26 +0100

    Remove example variables and fix sorted_variables

--- app/main.py
+++ app/main.py
@@ -15,10 +15,6 @@ marks = {
     "Sergio": 3,
 }
 collection_of_coins = {1, 2, 25}
-a = 123
-b = []
-c = "Hi!"
-d = [1, 2]
 
 sorted_variables = {
     "mutable": [collection_of_coins, marks, my_favourite_films
